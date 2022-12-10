24df51638b4e1d919bdcd5d23d0091c4e0b97204
Nazma1997 <nazma.ist.707@gmail.com>
2022-12-10 23:19:47 +0600

    added

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -14,7 +14,7 @@ const Discover = () => {
      if(isFetching) return <Loader  title='Loading Songs'/>
 
      if(error) return <Error />
-  const genreTitle = 'Pop';
+  // const genreTitle = 'Pop';
 
   // const handlePauseClick = () => {
   //   dispatch(playPause(false))
@@ -31,14 +31,14 @@ const Discover = () => {
   return (
     <div className='flex flex-col'>
       <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
-        <h2 className='font-bold text-3xl text-white text-left mb-3'>Discover {genreTitle}</h2>
-        <select
+        <h2 className='font-bold text-3xl text-white text-left mb-3'>Discover your  song</h2>
+        {/* <select
          onChange={() => {}}
          value=''
          className='bg-black text-gray-300 p-3  text-sm rounded-lg outline-none sm: mt-0 mt-5'
         >
           {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
-        </select>
+        </select> */}
       </div>
 
       <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
